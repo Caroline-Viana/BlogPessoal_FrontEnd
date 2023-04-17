a79a9519f510491eee95ad58f9a7b07710c505cd
Caroline Viana <carolviana-21@hotmail.com>
2023-04-17 15:21:22 -0300

    Criando pagina de cadastro e integrando a aplicação com a api

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Navbar from './components/static/navbar/Navbar'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Footer from './components/static/footer/Footer'
+import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +20,7 @@ function App() {
           <Route path="/" element={<Login/>} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
         </Routes>
       </div>
       <Footer />
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,41 +1,105 @@
-import React from 'react'
+import React, { ChangeEvent, useEffect } from 'react'
 import './Login.css'
 import { Box, Grid, TextField, Typography, Button, ThemeProvider, createTheme } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { login } from '../../services/Service';
+import UserLogin from '../../models/UserLogin';
+import { useState } from 'react';
 
 const theme = createTheme({
     palette: {
-       primary: {
-          main: '#D29642',
-       },
-       secondary: {
-          main: '#B85851',
-       },
+        primary: {
+            main: '#D29642',
+        },
+        secondary: {
+            main: '#B85851',
+        },
     }
 });
 
 function Login() {
+
+    let navigate = useNavigate()
+    const [token, setToken] = useLocalStorage('token');
+    //[Acessar, alterar]
+    const [userLogin, setUserLogin] = useState<UserLogin>({
+        id: 0,
+        nome: '',
+        usuario: '',
+        senha: '',
+        foto: '',
+        token: ''
+    })
+
+    function updatedModel(evento: ChangeEvent<HTMLInputElement>) {
+        setUserLogin({
+            ...userLogin,
+            //[Capturando a propriedade] : Capturando o valor da propriedad
+            [evento.target.name]: evento.target.value
+        })
+    }
+
+    useEffect(() => {
+        if(token != ''){
+            navigate('/home')
+        }
+    }, [token])
+
+    async function onSumit(evento: ChangeEvent<HTMLFormElement>) {
+        evento.preventDefault();
+        try {
+            await login('/usuarios/logar', userLogin, setToken)
+            alert("Usuario logado")
+        } catch (error) {
+            alert("Erro ao logar")
+        }
+    }
+
     return (
         <Grid container className='container'>
             <Grid xs={6} className='container_form'>
                 <Box className='form'>
-                    <form className='form_opcao'>
+                    <form className='form_opcao' onSubmit={onSumit}>
                         <Typography variant='h3' component='h3' className='opcao_titulo'>Entrar</Typography>
-                        <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth></TextField>
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' type='password' margin='normal' fullWidth className='campo'></TextField>
+                        <TextField
+                            id='usuario'
+                            label='usuário'
+                            variant='outlined'
+                            name='usuario'
+                            margin='normal'
+                            fullWidth
+                            value={userLogin.usuario}
+                            onChange={(evento:ChangeEvent<HTMLInputElement>) => updatedModel(evento)}>
+                        </TextField>
+
+                        <TextField
+                            id='senha'
+                            label='senha'
+                            variant='outlined'
+                            name='senha'
+                            type='password'
+                            margin='normal'
+                            fullWidth
+                            className='campo'
+                            value={userLogin.senha}
+                            onChange={(evento:ChangeEvent<HTMLInputElement>) => updatedModel(evento)}>
+                        </TextField>
                         <Box textAlign='center'>
-                            <Link to='/home' className='text-decorator-none'>
-                            <ThemeProvider theme={theme}>
-                                <Button type='submit' variant='contained' fullWidth color='primary' className='botao'>Logar</Button>
-                            </ThemeProvider>
-                            </Link>
+                            
+                                <ThemeProvider theme={theme}>
+                                    <Button type='submit' variant='contained' fullWidth color='primary' className='botao'>Logar</Button>
+                                </ThemeProvider>
+                            
                         </Box>
                     </form>
                     <Box className='form_opcao' display='flex' justifyContent='center'>
                         <Box marginRight={1}>
                             <Typography variant='subtitle1' align='center'>Não tem uma conta?</Typography>
                         </Box>
-                        <Typography variant='subtitle1' align='center' style={{fontWeight: 'bold'}}>Cadastre-se</Typography>
+                        <Link to='/cadastrousuario'>
+                            <Typography variant='subtitle1' align='center' style={{ fontWeight: 'bold' }}>Cadastre-se</Typography>
+                        </Link>
                     </Box>
                 </Box>
             </Grid>
